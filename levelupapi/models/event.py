6192ca6413bff9f5b550a75405e1d359b7ed9e61
jakefroeb <jake.herman.froeb@gmail.com>
2021-05-05 12:52:46 -0500

    added signup and updated the list views in the viewset also added a property joined to event model

--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -6,3 +6,11 @@ class Event(models.Model):
     date = models.DateField(auto_now=False, auto_now_add=False)
     game = models.ForeignKey(Game, on_delete=models.CASCADE)
     host = models.ForeignKey(Gamer, on_delete=models.CASCADE)
+
+    @property
+    def joined(self):
+        return self.__joined
+
+    @joined.setter
+    def joined(self, value):
+        self.__joined = value
